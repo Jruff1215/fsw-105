027beee2dab970b63d43354ecb1031bdaa25450d
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-02-03 00:04:42 -0500

    function commit

---
+++ week3/functions/index.js
@@ -0,0 +1,21 @@
+function add(a, b){
+    return a + b;
+}
+var sum = add(1, 2); {
+    console.log(sum)
+}
+var numbers = [ 2, 4, 6];
+var max = Math.max(2, 4, 6)
+    console.log(max)
+
+var i = 1
+i % 2 == 0 ? $Result = "Even" : $Result = "Odd";
+console.log($Result)
+
+var str = "This assignment really is something";
+if (str <= str.substring(0, 20)) {
+    console.log(str + "" + str)
+}
+else {
+    console.log(str.substring(0, 17))
+}
