67226c683aef0095601ab0ba949e574f83841d0d
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-02-02 20:22:50 -0500

    loopArray Commit

---
+++ week3/loopsAndArrays/index.js
@@ -0,0 +1,44 @@
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    },{
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    },{
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    },{
+        name: "Sam",
+        age: 30,
+        gender: "male"
+    },{
+        name: "Suzy",
+        age: 4,
+        gender: "female"
+    }
+]
+if ("Mike" >= 18){
+    console.log("Mike is old enough to see Mad Max")
+}
+else if ("Mike" >= 18 + "male"){
+    console.log("Mike is not old enough to see Mad Max, dont let HIM in")
+}
+else {
+    console.log("Mike is not old enough to see Mad Max")
+}
+var i
+for (i =0; i < 101; i++){
+    console.log(i)
+    i %  2 == 0 ? $Result = "Even" : $Result = "Odd";
+    console.log($Result)
+}
+var numbers = [45, 4, 9, 16, 25];
+var allOver18 = numbers.every(myFunction);
+
+function myFunction(45, allOver18, numbers){
+    return 45 > 18;
+}
