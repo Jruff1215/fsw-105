04981575a3e086152cb4db0a4e7cf82eca1ecf97
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-02-08 00:29:09 -0500

    Fixed Errors Commit

--- week3/functions/index.js
+++ week3/functions/index.js
@@ -4,14 +4,25 @@ function add(a, b){
 var sum = add(1, 2); {
     console.log(sum)
 }
+function number(a, b, c) {
+}
+function getMaxNumber(a, b, c){
+    return Math.max(a, b, c)
+}
 var numbers = [ 2, 4, 6];
 var max = Math.max(2, 4, 6)
     console.log(max)
 
+function evenOrOdd (a) {
+    return $Result
+}
 var i = 1
 i % 2 == 0 ? $Result = "Even" : $Result = "Odd";
 console.log($Result)
 
+function string(){
+    return (string + string)
+}
 var str = "This assignment really is something";
 if (str <= str.substring(0, 20)) {
     console.log(str + "" + str)
---
+++ week4/stringArrayMethods/index.js
@@ -0,0 +1,20 @@
+var capitalizeAndLowercase ="Hello"
+var a = capitalizeAndLowercase.toUpperCase();
+var b = capitalizeAndLowercase.toLowerCase();
+console.log(a + " " + b)
+
+
+var d = "Hello World"
+console.log(d.slice(0, 5))
+
+function capitalAndLowercase(){
+var e = "Hello World"
+var f = e.slice(0, 5);
+var g = f.toUpperCase();
+var h = e.slice(5, 11);
+var i = h.toLowerCase();
+return 
+}
+console.log()
+
+
