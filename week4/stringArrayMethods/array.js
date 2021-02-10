68286c45bd28261b3aa721daf5af3836fcf06a39
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-02-10 11:30:34 -0500

    array methods

--- week4/stringArrayMethods/array.js
+++ week4/stringArrayMethods/array.js
@@ -1,4 +1,27 @@
 var fruit = ["banana", "apple", "orange", "watermelon"];
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
 
-function removeLastFruit()
+function arrayChanges(){
+    return changes
+}
+var a = (vegetables.pop());
+
+var b = (fruit.shift());
+
+fruit.forEach(function(indexElement, index, array){
+});
+
+var c = fruit.push("1");
+
+var d = vegetables.length;
+
+var e = vegetables.push("3");
+
+var food = fruit.concat(vegetables);
+
+var removed = food.splice(4, 2);
+
+var reverse = food.reverse();
+
+var string = food.join();
+console.log("The array is now a string: " + string)
