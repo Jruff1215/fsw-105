b9c4fc49f33337da6423f3cf38f2e281c5c9cce5
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-02-10 00:38:39 -0500

    fixed strings

--- week4/stringArrayMethods/index.js
+++ week4/stringArrayMethods/index.js
@@ -1,25 +1,29 @@
-function capitalizeAndLowercase(A, b){
+function capitalizeAndLowercase(b, c){
     return b + c
 }
 var a ="Hello"
 var b = a.toUpperCase();
 var c = a.toLowerCase();
-console.log(b + " " + c)
+console.log(b + " " + c);
+capitalizeAndLowercase(b, c)
 
-function findMiddleIndex(str){
+function findMiddleIndex(i){
     return Math.floor(i)
 }
 var i = 10.5
-console.log(Math.floor(i))
+console.log(Math.floor(i));
+findMiddleIndex(i);
 
-function slice(str) {
-    return str.slice()
+function slice(d, l) {
+    return l
 }
-var d = "Hello World"
-console.log(d.slice(0, 5))
+var d = "Hello World";
+var l = (d.slice(0, 5));
+console.log(l);
+slice(l);
 
-function capitalAndLowercase(str){
-    return STRing 
+function capitalAndLowercase(g, i){
+    return g + i
 }
 var e = "Hello World"
 var f = e.slice(0, 5);
@@ -27,5 +31,5 @@ var g = f.toUpperCase();
 var h = e.slice(5, 11);
 var i = h.toLowerCase();
 
-console.log(g + i)
-
+console.log(g + i);
+capitalAndLowercase(g, i)
